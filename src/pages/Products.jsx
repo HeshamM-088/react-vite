849dc68c330a9de00bd0408667efc7c5c7d0ebec
HeshamM-088 <heshamkhalil1988@gmail.com>
2024-02-29 16:56:17 +0200

    second

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
   return <div>Products</div>;
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Users = () => {
-  return <div>Users</div>;
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const info = await res.json();
+
+    setUsers(info);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center gap-3">
+      <h1>Users</h1>
+
+      <section className="d-flex flex-column bg-dark p-5 align-items-center justify-content-center gap-3 rounded ">
+        {users.map((user, index) => (
+          <div
+            key={index}
+            className="d-flex align-items-center justify-content-evenly bg-success text-light fw-bold w-100 rounded p-3 gap-5"
+          >
+            <span>{user.id}</span>
+            <span>UserName : {user.username}</span>
+            <span>Email : {user.email}</span>
+            <span>Website : {user.website}</span>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default Users;
